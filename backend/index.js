ce2f064d8376354bdf70cfe42590385c9fcf9720
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-08-04 17:54:12 +0530

    changed db connection appraoch

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5080;
 const bcrypt = require('bcryptjs');
 
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGO_DB_URI;
 const corsOptions = {
   origin:["https://admin-dash-board-api.vercel.app/","http://localhost:5173","http://localhost:5080/","https://admin-dash-board-indol-six.vercel.app/"]
@@ -13,79 +13,84 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+
 app.get("/", (req,res) => {
     res.send("Hello World!");
 })
 
-MongoClient.connect(uri)
-.then(clientObj => {
-  const users = clientObj.db("DealsDrayTest").collection("users");
-  const employees = clientObj.db("DealsDrayTest").collection("employees");
-  console.log("Mongo DB connected successfully!");
-})
-.catch(err =>{
-  console.log(err)
-})
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-  app.post("/register-user",async (req,res) => {
-      try {
-        const {username,password} = req.body;
-        console.log("Signup :",username,password)
-        const hashedPassword = await bcrypt.hash(password,10);
-        const User = await users.insertOne({username,password:hashedPassword,empList:[]});
-        if(!User){
-          return res.status(501).json("Error creating user!")
+async function run() {
+  try {
+    await client.connect();
+
+    const users = client.db("DealsDrayTest").collection("users");
+    const employees = client.db("DealsDrayTest").collection("employees");
+
+    app.post("/register-user",async (req,res) => {
+        try {
+          const {username,password} = req.body;
+          console.log("Signup :",username,password)
+          const hashedPassword = await bcrypt.hash(password,10);
+          const User = await users.insertOne({username,password:hashedPassword,empList:[]});
+          if(!User){
+            return res.status(501).json("Error creating user!")
+          }
+          const newUser = await users.findOne({_id : new ObjectId(User.insertedId)});
+          res.status(200).json(newUser);
+        } catch (error) {
+          console.log("Error :",error);
+          res.status(500).json("Internal server error!");
         }
-        const newUser = await users.findOne({_id : new ObjectId(User.insertedId)});
-        res.status(200).json(newUser);
-      } catch (error) {
-        console.log("Error :",error);
-        res.status(500).json("Internal server error!");
+    })
+
+    app.post("/login-user",async (req,res) => {
+      try{
+          const {username,password} = req.body;
+          console.log(username,password)
+          const UserDetails = await users.findOne({username : username});
+          console.log("User:",UserDetails)
+          if(!UserDetails){
+              return res.status(404).json("User not found! Please Create an account and try again...")
+          }
+          const passwordMatch = await bcrypt.compare(password,UserDetails.password);
+          if(!passwordMatch){
+              return res.status(401).json("Password was incorrect!")
+          }else{
+              console.log(UserDetails)
+              return res.status(200).json(UserDetails);
+          }
+      }catch(error){
+          res.status(500).json({message : "Internal server error!"})
       }
   })
 
-  app.post("/login-user",async (req,res) => {
-    try{
-        const {username,password} = req.body;
-        console.log(username,password)
-        const UserDetails = await users.findOne({username : username});
-        console.log("User:",UserDetails)
-        if(!UserDetails){
-            return res.status(404).json("User not found! Please Create an account and try again...")
-        }
-        const passwordMatch = await bcrypt.compare(password,UserDetails.password);
-        if(!passwordMatch){
-            return res.status(401).json("Password was incorrect!")
-        }else{
-            console.log(UserDetails)
-            return res.status(200).json(UserDetails);
-        }
-    }catch(error){
-        res.status(500).json({message : "Internal server error!"})
-    }
-  })
-
-  app.post("/add/employee/:username",async (req,res) => {
-    try{
-        const empData = req.body;
-        const username = req.params.username;
-        const existingEmployee = await employees.findOne({empName:empData.empName});
-        if(existingEmployee){
-          return res.status(403).json(`Employee with ${empData.email} exists, please enter another email!`)
-        }
-        const newEmployee = await employees.insertOne(empData);
-        console.log("new :",newEmployee)
-        console.log(employees)
-        if(!newEmployee){
-            return res.status(501).json("Error creating new employee!")
-        }
-        const empDetails = await employees.findOne({_id : new ObjectId(newEmployee.insertedId)});
-        return res.status(200).json(empDetails);
-        
-    }catch(error){
-        res.status(500).json({message : "Internal server error!"})
-    }
+    app.post("/add/employee/:username",async (req,res) => {
+      try{
+          const empData = req.body;
+          const username = req.params.username;
+          const existingEmployee = await employees.findOne({empName:empData.empName});
+          if(existingEmployee){
+            return res.status(403).json(`Employee with ${empData.email} exists, please enter another email!`)
+          }
+          const newEmployee = await employees.insertOne(empData);
+          console.log("new :",newEmployee)
+          console.log(employees)
+          if(!newEmployee){
+              return res.status(501).json("Error creating new employee!")
+          }
+          const empDetails = await employees.findOne({_id : new ObjectId(newEmployee.insertedId)});
+          return res.status(200).json(empDetails);
+          
+      }catch(error){
+          res.status(500).json({message : "Internal server error!"})
+      }
   })
 
   app.get('/get/employee/:employeeId', async (req,res) => {
@@ -146,9 +151,19 @@ MongoClient.connect(uri)
       res.status(500).json("Internal server error!")
     }
   })
+  
+  
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port,() => {
-  console.log(`App listening on port number : ${port}`);
-})
+      console.log(`App listening on port number : ${port}`);
+  })
 
 module.exports = app
