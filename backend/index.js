e886cd11d35a77c05c68462c95baa5c426bb279d
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-08-07 18:25:37 +0530

    addex loading and adjusted backend logic

--- backend/index.js
+++ backend/index.js
@@ -27,16 +27,137 @@ const client = new MongoClient(uri, {
   }
 });
 
-let users,employees;
 
 async function run() {
   try {
     await client.connect();
 
-     users = client.db("DealsDrayTest").collection("users");
-     employees = client.db("DealsDrayTest").collection("employees");
-  
-
+     const users = client.db("DealsDrayTest").collection("users");
+     const employees = client.db("DealsDrayTest").collection("employees");
+    
+     app.post("/register-user",async (req,res) => {
+      try {
+        const {username,password} = req.body;
+        console.log(username,password)
+        const hashedPassword = await bcrypt.hash(password,10);
+        const existingUser = await users.findOne({username: username});
+        console.log(existingUser)
+        if(existingUser){
+          return res.status(403).json("User exists with similar credentials!")
+        }
+        const User = await users.insertOne({username,password:hashedPassword,empList:[]});
+        if(!User){
+          return res.status(501).json("Error creating user!")
+        }
+        const newUser = await users.findOne({_id : new ObjectId(User.insertedId)});
+        res.status(200).json(newUser);
+      } catch (error) {
+        console.log("Error :",error);
+        res.status(500).json("Internal server error!");
+      }
+    })
+    
+    app.post("/login-user",async (req,res) => {
+    try{
+        const {username,password} = req.body;
+        console.log(req.body)
+        const UserDetails = await users.findOne({username : username});
+        if(!UserDetails){
+            return res.status(404).json("User not found! Please Create an account and try again...")
+        }
+        const passwordMatch = await bcrypt.compare(password,UserDetails.password);
+        if(!passwordMatch){
+            return res.status(401).json("Password was incorrect!")
+        }else{
+            console.log(UserDetails)
+            return res.status(200).json(UserDetails);
+        }
+    }catch(error){
+        res.status(500).json({message : "Internal server error!"})
+    }
+    })
+    
+    app.post("/add/employee/:username",async (req,res) => {
+    try{
+        const empData = req.body;
+        const username = req.params.username;
+        const existingEmployee = await employees.findOne({empName:empData.empName});
+        if(existingEmployee){
+          return res.status(403).json(`Employee with ${empData.email} exists, please enter another email!`)
+        }
+        const newEmployee = await employees.insertOne(empData);
+        console.log("new :",newEmployee)
+        console.log(employees)
+        if(!newEmployee){
+            return res.status(501).json("Error creating new employee!")
+        }
+        const empDetails = await employees.findOne({_id : new ObjectId(newEmployee.insertedId)});
+        return res.status(200).json(empDetails);
+        
+    }catch(error){
+        res.status(500).json({message : "Internal server error!"})
+    }
+    })
+    
+    app.get('/get/employee/:employeeId', async (req,res) => {
+    try {
+    const EmployeeId = req.params.employeeId;
+    const EmployeeDetails = await employees.findOne({_id : new ObjectId(EmployeeId)});
+    console.log(EmployeeDetails)
+    if(!EmployeeDetails){
+      return res.status(404).json("No Employee exists with that id!");
+    }
+    res.status(200).json(EmployeeDetails);
+    } catch (error) {
+    res.status(500).json("Internal server error!");
+    }
+    })
+    
+    app.patch('/update/employee/:employeeId', async (req,res) => {
+    try {
+    const EmployeeId = req.params.employeeId;
+    const empData = req.body;
+    const UpdatedEmployee = await employees.findOneAndUpdate(
+      {_id : new ObjectId(EmployeeId)},
+      {$set: {...empData}},
+      {upsert:true}
+    );
+    console.log(UpdatedEmployee)
+    if(!UpdatedEmployee){
+      return res.status(501).json("Error in updating the employee!");
+    }
+    res.status(200).json(UpdatedEmployee);
+    } catch (error) {
+    res.status(500).json("Internal server error!");
+    }
+    })
+    
+    app.get('/get/employees/:username', async (req,res) => {
+    try {
+    const username = req.params.username;
+    const Employees = await employees.find({}).toArray();
+    if(!Employees){
+      return res.status(404).json("No Employees exists!");
+    }
+    res.status(200).json(Employees);
+    } catch (error) {
+    res.status(500).json("Internal server error!");
+    }
+    })
+    
+    app.delete("/delete/employee/:employeeId", async (req,res) => {
+    try {
+    const EmployeeId = req.params.employeeId;
+    const DeletedEmployee = await employees.deleteOne({_id : new ObjectId(EmployeeId)});
+    if(!DeletedEmployee){
+      res.status(501).json("Error deleting the employee!");
+    }
+    res.status(200).json(DeletedEmployee)
+    } catch (error) {
+    res.status(500).json("Internal server error!")
+    }
+    })
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -44,132 +165,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-app.post("/register-user",async (req,res) => {
-  try {
-    const {username,password} = req.body;
-    console.log(username,password)
-    const hashedPassword = await bcrypt.hash(password,10);
-    const existingUser = await users.findOne({username: username});
-    console.log(existingUser)
-    if(existingUser){
-      return res.status(403).json("User exists with similar credentials!")
-    }
-    const User = await users.insertOne({username,password:hashedPassword,empList:[]});
-    if(!User){
-      return res.status(501).json("Error creating user!")
-    }
-    const newUser = await users.findOne({_id : new ObjectId(User.insertedId)});
-    res.status(200).json(newUser);
-  } catch (error) {
-    console.log("Error :",error);
-    res.status(500).json("Internal server error!");
-  }
-})
-
-app.post("/login-user",async (req,res) => {
-try{
-    const {username,password} = req.body;
-    console.log(req.body)
-    const UserDetails = await users.findOne({username : username});
-    if(!UserDetails){
-        return res.status(404).json("User not found! Please Create an account and try again...")
-    }
-    const passwordMatch = await bcrypt.compare(password,UserDetails.password);
-    if(!passwordMatch){
-        return res.status(401).json("Password was incorrect!")
-    }else{
-        console.log(UserDetails)
-        return res.status(200).json(UserDetails);
-    }
-}catch(error){
-    res.status(500).json({message : "Internal server error!"})
-}
-})
-
-app.post("/add/employee/:username",async (req,res) => {
-try{
-    const empData = req.body;
-    const username = req.params.username;
-    const existingEmployee = await employees.findOne({empName:empData.empName});
-    if(existingEmployee){
-      return res.status(403).json(`Employee with ${empData.email} exists, please enter another email!`)
-    }
-    const newEmployee = await employees.insertOne(empData);
-    console.log("new :",newEmployee)
-    console.log(employees)
-    if(!newEmployee){
-        return res.status(501).json("Error creating new employee!")
-    }
-    const empDetails = await employees.findOne({_id : new ObjectId(newEmployee.insertedId)});
-    return res.status(200).json(empDetails);
-    
-}catch(error){
-    res.status(500).json({message : "Internal server error!"})
-}
-})
-
-app.get('/get/employee/:employeeId', async (req,res) => {
-try {
-const EmployeeId = req.params.employeeId;
-const EmployeeDetails = await employees.findOne({_id : new ObjectId(EmployeeId)});
-console.log(EmployeeDetails)
-if(!EmployeeDetails){
-  return res.status(404).json("No Employee exists with that id!");
-}
-res.status(200).json(EmployeeDetails);
-} catch (error) {
-res.status(500).json("Internal server error!");
-}
-})
-
-app.patch('/update/employee/:employeeId', async (req,res) => {
-try {
-const EmployeeId = req.params.employeeId;
-const empData = req.body;
-const UpdatedEmployee = await employees.findOneAndUpdate(
-  {_id : new ObjectId(EmployeeId)},
-  {$set: {...empData}},
-  {upsert:true}
-);
-console.log(UpdatedEmployee)
-if(!UpdatedEmployee){
-  return res.status(501).json("Error in updating the employee!");
-}
-res.status(200).json(UpdatedEmployee);
-} catch (error) {
-res.status(500).json("Internal server error!");
-}
-})
-
-app.get('/get/employees/:username', async (req,res) => {
-try {
-const username = req.params.username;
-const Employees = await employees.find({}).toArray();
-if(!Employees){
-  return res.status(404).json("No Employees exists!");
-}
-res.status(200).json(Employees);
-} catch (error) {
-res.status(500).json("Internal server error!");
-}
-})
-
-app.delete("/delete/employee/:employeeId", async (req,res) => {
-try {
-const EmployeeId = req.params.employeeId;
-const DeletedEmployee = await employees.deleteOne({_id : new ObjectId(EmployeeId)});
-if(!DeletedEmployee){
-  res.status(501).json("Error deleting the employee!");
-}
-res.status(200).json(DeletedEmployee)
-} catch (error) {
-res.status(500).json("Internal server error!")
-}
-})
-
-
-
 
 app.listen(port,() => {
       console.log(`App listening on port number : ${port}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,32 @@ import './App.css'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import { Outlet, useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const navigate = useNavigate();
+  const [loading,setLoading] = useState();
+
   useEffect(() => {
-    if(!localStorage.getItem("username")){
-      navigate("/signup");
-    }
-  })
+    setLoading(true)
+    setTimeout(() =>{
+      if(!localStorage.getItem("username") && location.href!=="https://admin-dash-board-indol-six.vercel.app/"){
+        navigate("/signup");
+        setLoading(false);
+      }
+    },2000);
+  },[location.href,localStorage.getItem("username")])
+
+  if(loading){
+    return <div className=" flex flex-col items-center justify-center h-screen">
+      <div className="relative">
+          <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+          <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
+          </div>
+      </div>
+      Loading...Please Wait...
+  </div>
+  }
 
   return (
     <div className=''>
